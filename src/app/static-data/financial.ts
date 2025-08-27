720f4ef08624d9d04ba4b9b3624d9ae061773b01
Gabriel Martínez <GMartinez@infragistics.com>
2025-08-27 13:58:18 -0300

    Update sample data

--- src/app/static-data/financial.ts
+++ src/app/static-data/financial.ts
@@ -1,15 +1,11 @@
 // tslint:disable
 export const Financial: { [key: string]: any } = {
   'BoxOfficeRevenueType': [
-    { "Franchise": "Cell 0.1", "TotalWorldBoxOfficeRevenue": 0, "HighestGrossingMovieInSeries": 0 },
-    { "Franchise": "Cell 1.1", "TotalWorldBoxOfficeRevenue": 1, "HighestGrossingMovieInSeries": 1 },
-    { "Franchise": "Cell 2.1", "TotalWorldBoxOfficeRevenue": 2, "HighestGrossingMovieInSeries": 2 },
-    { "Franchise": "Cell 3.1", "TotalWorldBoxOfficeRevenue": 3, "HighestGrossingMovieInSeries": 3 },
-    { "Franchise": "Cell 4.1", "TotalWorldBoxOfficeRevenue": 4, "HighestGrossingMovieInSeries": 4 },
-    { "Franchise": "Cell 5.1", "TotalWorldBoxOfficeRevenue": 5, "HighestGrossingMovieInSeries": 5 },
-    { "Franchise": "Cell 6.1", "TotalWorldBoxOfficeRevenue": 6, "HighestGrossingMovieInSeries": 6 },
-    { "Franchise": "Cell 7.1", "TotalWorldBoxOfficeRevenue": 7, "HighestGrossingMovieInSeries": 7 },
-    { "Franchise": "Cell 8.1", "TotalWorldBoxOfficeRevenue": 8, "HighestGrossingMovieInSeries": 8 },
-    { "Franchise": "Cell 9.1", "TotalWorldBoxOfficeRevenue": 9, "HighestGrossingMovieInSeries": 9 }
+    { "Franchise": "Marvel", "TotalWorldBoxOfficeRevenue": 22.55, "HighestGrossingMovieInSeries": 2.8 },
+    { "Franchise": "Star Wars", "TotalWorldBoxOfficeRevenue": 10.32, "HighestGrossingMovieInSeries": 2.07 },
+    { "Franchise": "Harry Potter", "TotalWorldBoxOfficeRevenue": 9.19, "HighestGrossingMovieInSeries": 1.34 },
+    { "Franchise": "Avengers", "TotalWorldBoxOfficeRevenue": 7.76, "HighestGrossingMovieInSeries": 2.8 },
+    { "Franchise": "Spider Man", "TotalWorldBoxOfficeRevenue": 7.22, "HighestGrossingMovieInSeries": 1.28 },
+    { "Franchise": "James Bond", "TotalWorldBoxOfficeRevenue": 7.12, "HighestGrossingMovieInSeries": 1.11 }
   ],
 };
